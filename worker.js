a69c55c15e33695db42454b828e813d09a713485
Ben <blearn5241@gmail.com>
2022-12-17 16:31:37 -0500

    debugging

--- worker.js
+++ worker.js
@@ -8,7 +8,7 @@ let workers = process.env.WEB_CONCURRENCY || 1;
 function start() {
   // Connect to the named work queue
   let workQueue = new Queue('work', process.env.REDIS_URL);
-
+  console.log('Worker started.');
   workQueue.process('kickoff', async (job) => {
     console.log('Kickoff job received');
     await orchestrate(job.data);
