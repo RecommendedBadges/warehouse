fece4814877627a2690a81957bd4948b34513dad
Ben <blearn5241@gmail.com>
2022-12-19 21:03:18 -0500

    fixing export syntax in util

--- util/heroku.js
+++ util/heroku.js
@@ -11,4 +11,6 @@ async function scaleClockDyno(numDynos) {
     );
 }
 
-module.exports = scaleClockDyno;
+module.exports = {
+    scaleClockDyno
+};
--- util/index.js
+++ util/index.js
@@ -4,11 +4,4 @@ const github = require('./github.js');
 const heroku = require('./heroku.js');
 const sfdx = require('./sfdx.js');
 
-module.exports = {
-    authorize: sfdx.authorize,
-    callout,
-    fatal: error.fatal,
-    github,
-    getRemainingPackageNumber: sfdx.getRemainingPackageNumber,
-    heroku
-};
+module.exports = Object.assign({}, callout, error, github, heroku, sfdx);
