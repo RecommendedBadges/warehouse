a5442fff92436140f0604479e9a5508b9d31fe33
Ben <blearn5241@gmail.com>
2023-01-15 17:02:51 -0500

    removing console.log() statements

--- util/callout.js
+++ util/callout.js
@@ -5,9 +5,6 @@ const { fatal } = require('./error.js');
 
 async function get({site, endpoint, fullUrl}) {
     try {
-        console.log(site);
-        console.log(endpoint);
-        console.log(fullUrl);
         const res = await axios.get(
             fullUrl ? fullUrl : `${common.API_BASES[site]}${endpoint}`,
             {
