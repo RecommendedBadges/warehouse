e6810dbb0948e7f1f0c626b52e5114782dd9acdd
Ben <blearn5241@gmail.com>
2024-06-23 20:26:57 -0400

    moved worker/clock functionality to warehouse-worker app

--- config/index.js
+++ config/index.js
@@ -1,6 +1,4 @@
 const common = require('./components/common.js');
-const git = require('./components/git.js');
 const github = require('./components/github.js');
-const sfdx = require('./components/sfdx.js');
 
-module.exports = Object.assign({}, common, git, github, sfdx);
+module.exports = Object.assign({}, common, github);
